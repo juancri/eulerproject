54c2db3702c1dbd72c30bdb0c4d541e84a3a7de2
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 20:58:16 -0300

    Adding problem 19

---
+++ eulerproject/Problems/Problem19.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (19, "Counting Sundays", 171)]
+	public class Problem19 : IProblem
+	{
+		public long Run ()
+		{
+			return (from year in Enumerable.Range (1901, 100)
+			        from month in Enumerable.Range (1, 12)
+			        let date = new DateTime (year, month, 1)
+					where date.DayOfWeek == DayOfWeek.Sunday
+			        select 1).Count ();
+		}
+	}
+}
+
