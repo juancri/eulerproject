3d53b82d50a1dc8badb4b971e8e76cce8b83aaae
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 17:29:59 -0300

    Adding problem 17

---
+++ eulerproject/Problems/Problem17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (17, "Number letter counts", 1)]
+	public class Problem17 : IProblem
+	{
+		public long Run ()
+		{
+			Console.WriteLine (1000.ToWords ());
+			return Enumerable.Range (1, 1000)
+				.Select (n => n.ToWords ())
+				.Select (w => w.Length)
+				.Sum ();
+		}
+	}
+}
+
---
+++ eulerproject/Util/NumberToWords.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace eulerproject
+{
+	public static class NumberToWords
+	{
+		private static readonly string [] firstTwenty =
+			new []
+			{
+				"one", "two", "three", "four", "five", "six",
+				"seven", "eight", "nine", "ten", "eleven",
+				"twelve", "thirteen", "fourteen", "fifteen",
+				"sixteen", "seventeen", "eighteen", "nineteen"
+			};
+		private static readonly string [] decimals =
+			new []
+			{
+				
+				"twenty", "thirty", "forty", "fifty", "sixty",
+				"seventy", "eighty", "ninety"
+			};
+
+		public static string ToWords (this int n)
+		{
+			if (n == 0)
+				return string.Empty;
+			
+			if (n < 20)
+				return firstTwenty [n - 1];
+			
+			if (n < 100)
+				return decimals [(n / 10) - 2] + (n % 10).ToWords ();
+
+			if (n < 1000)
+				return (n % 100 > 0) ?
+					(n / 100).ToWords () + "hundredand" + (n % 100).ToWords () :
+					(n / 100).ToWords () + "hundred";
+			if (n < 1000000)
+				return (n / 1000).ToWords () + "thousand" + (n % 1000).ToWords ();
+
+			throw new NotImplementedException ("Not implemented for: " + n);
+		}
+	}
+}
+
