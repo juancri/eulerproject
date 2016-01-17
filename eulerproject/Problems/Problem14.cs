f5b68efaeda62d93e2141670c3994c0a8db02ba8
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 15:35:36 -0300

    Adding problem 14

---
+++ eulerproject/Problems/Problem14.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (14, "Longest Collatz sequence", 1)]
+	public class Problem14 : IProblem
+	{
+		public long Run ()
+		{
+			var numbers = new Dictionary <long, int> ();
+			foreach (var x in Enumerable.Range (1, 999999))
+			{
+				var local = new List <long> ();
+				var distance = 0;
+				foreach (var y in CollatzSequence.Get (x))
+				{
+					if (numbers.ContainsKey (y))
+					{
+						distance = numbers [y];
+						break;
+					}
+					else
+						local.Add (y);
+				}
+
+				local.Reverse ();
+				for (var i = 0; i < local.Count; i++)
+					numbers.Add (local [i], i + distance + 1);
+			}
+			return
+				numbers
+					.OrderByDescending (n => n.Value)
+					.First ()
+					.Key;
+		}
+	}
+}
+
---
+++ eulerproject/Util/CollatzSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eulerproject
+{
+	public static class CollatzSequence
+	{
+		public static IEnumerable <long> Get (long start)
+		{
+			while (true)
+			{
+				yield return start;
+				if (start == 1)
+					yield break;
+				
+				if ((start & 1) == 0)
+					start = start / 2;
+				else
+					start = start * 3 + 1;
+			}
+		}
+	}
+}
+
