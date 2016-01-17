1ecd49b031747b55c2c090e9593528ba36674047
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 16:54:01 -0300

    Adding problem 15

---
+++ eulerproject/Problems/Problem15.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace eulerproject
+{
+	[Problem (15, "Next Lattice paths", 137846528820)]
+	public class Problem15 : IProblem
+	{
+		public long Run ()
+		{
+			return LatticePaths.Get (20);
+		}
+	}
+}
+
---
+++ eulerproject/Util/LatticePaths.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eulerproject
+{
+	public static class LatticePaths
+	{
+		public static long Get (int size)
+		{
+			return Get (
+				size, size,
+				new Dictionary <Tuple <int, int>, long> ());
+		}
+
+		private static long Get (int sizeA, int sizeB, Dictionary <Tuple <int, int>, long> cache)
+		{
+			// Normalize
+			var normalizedSizeA = Math.Min (sizeA, sizeB);
+			var normalizedSizeB = Math.Max (sizeA, sizeB);
+			var tuple = new Tuple <int, int> (normalizedSizeA, normalizedSizeB);
+
+			// Check cache
+			if (cache.ContainsKey (tuple))
+				return cache [tuple];
+
+			// Calculate
+			var possible = sizeA <= 1 || sizeB <= 1 ? 
+				sizeA + sizeB :
+				Get (sizeA, sizeB - 1, cache) +
+				Get (sizeA - 1, sizeB, cache);
+
+			// Save
+			cache.Add (tuple, possible);
+
+			// Done
+			return possible;
+		}
+	}
+}
+
