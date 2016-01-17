8bcbca1930ccffad3ba33f6ed2329e3eb4196b87
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 16:57:06 -0300

    Adding problem 16

---
+++ eulerproject/Problems/Problem16.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (16, "Power digit sum", 1)]
+	public class Problem16 : IProblem
+	{
+		public long Run ()
+		{
+			return BigInteger.Pow (new BigInteger (2), 1000)
+				.ToString ()
+				.Select (c => int.Parse (c.ToString ()))
+				.Sum ();
+		}
+	}
+}
+
