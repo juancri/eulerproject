823ad1e9c563a0905792064eaf4b5b43c22a7ccb
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 21:00:29 -0300

    FIX: formatting

--- eulerproject/Problems/Problem19.cs
+++ eulerproject/Problems/Problem19.cs
@@ -10,7 +10,7 @@ namespace eulerproject
 		{
 			return (from year in Enumerable.Range (1901, 100)
 			        from month in Enumerable.Range (1, 12)
-			        let date = new DateTime (year, month, 1)
+					let date = new DateTime (year, month, 1)
 					where date.DayOfWeek == DayOfWeek.Sunday
 			        select 1).Count ();
 		}
