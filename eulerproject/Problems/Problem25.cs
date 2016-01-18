4c57ef8b04ae3b248d4ae183b4c4c153a62d50a6
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 23:04:25 -0300

    Adding problem 25

---
+++ eulerproject/Problems/Problem25.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (25, "1000-digit Fibonacci number", 4782)]
+	public class Problem25 : IProblem
+	{
+		public long Run ()
+		{
+			return BigFibonacci.Get ()
+				.Select ((x, i) => new { number = x, index = i })
+				.Where (x => x.number.ToString ().Length == 1000)
+				.Select (x => x.index + 2)
+				.First ();
+		}
+	}
+}
+
---
+++ eulerproject/Util/BigFibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace eulerproject
+{
+	public static class BigFibonacci
+	{
+		public static IEnumerable <BigInteger> Get ()
+		{
+			var previous = new BigInteger (1);
+			var current = new BigInteger (1);
+			while (true)
+			{
+				yield return current;
+				var newcurrent = current + previous;
+				previous = current;
+				current = newcurrent;
+			}
+		}
+	}
+}
+
