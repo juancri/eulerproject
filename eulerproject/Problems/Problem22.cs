17aac3fd2703300e9612136e1e634fd6b605d85e
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-18 02:16:29 -0300

    Siplifying code

--- eulerproject/Problems/Problem22.cs
+++ eulerproject/Problems/Problem22.cs
@@ -6,25 +6,22 @@ namespace eulerproject
 	[Problem (22, "Names scores", 871198282)]
 	public class Problem22 : IProblem
 	{
+		private const string filename = "eulerproject.Assets.problem22-names.txt";
+
 		public long Run ()
 		{
 			return ReadFile ().Split (',')
 				.Select (n => n.Replace ("\"", string.Empty))
 				.OrderBy (n => n)
-				.Select ((n, i) => n.Select (GetCharacterValue).Sum () * (i + 1))
+				.Select ((n, i) => n.Select (c => c - 'A' + 1).Sum () * (i + 1))
 				.Sum ();
-		}
-
-		private static int GetCharacterValue (char c)
-		{
-			return c - 'A' + 1;
 		}
 
 		private static string ReadFile ()
 		{
 			using (var reader = new System.IO.StreamReader (
 				System.Reflection.Assembly.GetExecutingAssembly ()
-				.GetManifestResourceStream ("eulerproject.Assets.problem22-names.txt")))
+					.GetManifestResourceStream (filename)))
 				return reader.ReadToEnd ();
 		}
 	}
