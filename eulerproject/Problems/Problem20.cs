a9b6071e0c40c98951501076c037aee5d7d5626d
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 21:10:56 -0300

    Adding problem 20

---
+++ eulerproject/Problems/Problem20.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (20, "Factorial digit sum", 648)]
+	public class Problem20 : IProblem
+	{
+		public long Run ()
+		{
+			return BigFactorial.Get (100)
+				.ToString ()
+				.Select (c => int.Parse (c.ToString ()))
+				.Sum ();
+		}
+	}
+}
---
+++ eulerproject/Util/BigFactorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace eulerproject
+{
+	public static class BigFactorial
+	{
+		public static BigInteger Get (int n)
+		{
+			return Enumerable.Range (1, n)
+				.Select (x => new BigInteger (x))
+				.Aggregate (BigInteger.Multiply);
+		}
+	}
+}
+
