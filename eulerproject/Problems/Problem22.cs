df711e3ac3d15b6a1dfdc539d4be6b4ec66af303
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-17 22:29:01 -0300

    Adding problem 22 Adding problem 23

---
+++ eulerproject/Problems/Problem22.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (22, "Names scores", 871198282)]
+	public class Problem22 : IProblem
+	{
+		public long Run ()
+		{
+			return ReadFile ().Split (',')
+				.Select (n => n.Replace ("\"", string.Empty))
+				.OrderBy (n => n)
+				.Select ((n, i) => n.Select (GetCharacterValue).Sum () * (i + 1))
+				.Sum ();
+		}
+
+		private static int GetCharacterValue (char c)
+		{
+			return c - 'A' + 1;
+		}
+
+		private static string ReadFile ()
+		{
+			using (var reader = new System.IO.StreamReader (
+				System.Reflection.Assembly.GetExecutingAssembly ()
+				.GetManifestResourceStream ("eulerproject.Assets.problem22-names.txt")))
+				return reader.ReadToEnd ();
+		}
+	}
+}
+
---
+++ eulerproject/Problems/Problem23.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Slow]
+	[Problem (23, "Non-abundant sums", 4179871)]
+	public class Problem23 : IProblem
+	{
+		public long Run ()
+		{
+			var max = 28123;
+			var abundantNumbers = Enumerable.Range (1, max)
+				.Where (n => n < n.GetFactors ().Where (f => f != n).Sum ())
+				.ToList ();
+
+			var sums = 
+				(from a1 in abundantNumbers
+				from a2 in abundantNumbers
+				where a1 <= a2
+				let sum = a1 + a2
+				where sum <= max
+				select sum)
+					.Distinct ()
+					.ToList ();
+
+			return Enumerable.Range (1, max)
+				.Where (n => !sums.Contains (n))
+				.Sum ();
+		}
+	}
+}
+
