689596693a58e41d6561827886da6198d5eb62d3
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-21 03:51:31 -0300

    Adding problem 34

---
+++ eulerproject/Problems/Problem34.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (34, "Digit factorials", 40730)]
+	public class Problem34 : IProblem
+	{
+		public long Run ()
+		{
+			return Enumerable.Range (3, 100000)
+				.Where (n => n == n.GetDigits ()
+					.Select (d => d.GetFactorial ()).Sum ())
+				.Sum ();
+		}
+	}
+}
+
