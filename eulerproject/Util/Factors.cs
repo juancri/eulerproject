3d6403c0787900208a1825c7bae76e6f607a47ac
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-21 03:15:26 -0300

    Adding extension method IEnumerable <Tuple <int, int>> GetFactorPairs (this int n)

--- eulerproject/Util/Factors.cs
+++ eulerproject/Util/Factors.cs
@@ -23,6 +23,23 @@ namespace eulerproject
 				}
 			} while (a < b);
 		}
+
+		public static IEnumerable <Tuple <int, int>> GetFactorPairs (this int n)
+		{
+			var a = 0;
+			var b = 0;
+			var sqrt = Math.Sqrt (n);
+			do {
+				a++;
+				if (a > sqrt)
+					yield break;
+				if (n % a == 0) {
+					b = n / a;
+					yield return new Tuple <int, int> (a, b);
+					b--;
+				}
+			} while (a < b);
+		}
 	}
 }
 
