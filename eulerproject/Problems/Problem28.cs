34b272cfa571cfa996d98dbb8cfd570c13969577
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-20 21:59:38 -0300

    Adding problem 28

---
+++ eulerproject/Problems/Problem28.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (28, "Number spiral diagonals", 669171001)]
+	public class Problem28 : IProblem
+	{
+		public long Run ()
+		{
+			return GetDiagonals (1001)
+				.Sum ();	
+		}
+
+		private static IEnumerable<int> GetDiagonals (int size)
+		{
+			yield return 1;
+			var max = size * size;
+			var current = 1;
+			var step = 2;
+			while (current < max)
+			{
+				foreach (var n in Enumerable.Range (1, 4))
+					yield return current + step * n;
+
+				current += step * 4;
+				step += 2;
+			}
+		}
+	}
+}
+
