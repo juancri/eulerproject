bd0fe075159601a6a4d2ced9ba4fdf27ea8ef657
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-20 22:15:11 -0300

    Adding problem 30

---
+++ eulerproject/Problems/Problem30.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (30, "Digit fifth powers", 443839)]
+	public class Problem30 : IProblem
+	{
+		public long Run ()
+		{
+			return
+				Enumerable.Range (1000, 999000)
+					.Where (n => n ==
+						n.GetDigits ().Select (d => Math.Pow (d, 5)).Sum ())
+					.Sum ();
+		}
+	}
+}
+
