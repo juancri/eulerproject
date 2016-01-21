3175b511a12cb59b90c16796ed8a43855926d69f
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-20 22:03:58 -0300

    Adding problem 29

---
+++ eulerproject/Problems/Problem29.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (29, "Distinct powers", 9183)]
+	public class Problem29 : IProblem
+	{
+		public long Run ()
+		{
+			var seq =
+				from a in Enumerable.Range (2, 99)
+				from b in Enumerable.Range (2, 99)
+				select Math.Pow (a, b);
+
+			return seq.Distinct ().Count ();
+		}
+	}
+}
+
