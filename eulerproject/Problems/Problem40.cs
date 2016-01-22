4ea33c2191ee5b90aaad0558ccc920f8b44b7acd
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-21 22:54:40 -0300

    Adding problem 40

---
+++ eulerproject/Problems/Problem40.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace eulerproject
+{
+	[Problem (40, "Champernowne's constant", 210)]
+	public class Problem40 : IProblem
+	{
+		public long Run ()
+		{
+			var digits = Naturals.Get ()
+				.SelectMany (n => n.GetDigits ().Reverse ())
+				.Take (1000000)
+				.ToList ();
+
+			return digits [0] * digits [9] * digits [99] *
+				digits [999] * digits [9999] * digits [99999] *
+				digits [999999];
+		}
+	}
+}
+
