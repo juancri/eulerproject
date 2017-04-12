b8b35f077c05fc1bf171a7ad9cf4f83f48575ef6
Juan C. Olivares <cristobal@cxsoftware.com>
2017-04-12 03:21:31 -0300

    Adding new class Util/EratosthenesPrimes

---
+++ eulerproject/Util/EratosthenesPrimes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eulerproject
+{
+	public static class EratosthenesPrimes
+	{
+		public static IEnumerable <long> Get (long maximum)
+		{
+			var sqrt = Math.Sqrt (maximum);
+			var excluded = new bool[maximum];
+			var current = 1;
+			while (true)
+			{
+				current++;
+				if (current > maximum)
+					break;
+
+				if (!excluded [current - 1])
+					yield return current;
+
+				if (current > sqrt)
+					continue;
+
+				var temp = (long) Math.Pow (current, 2);
+				while (true)
+				{
+					if (temp > maximum)
+						break;
+					excluded[temp - 1] = true;
+					temp += current;
+				}
+			}
+		}
+	}
+}
