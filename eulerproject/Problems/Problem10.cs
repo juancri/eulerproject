3065d32898dd05ccec7c29150d3b156f9e4d972b
Juan C. Olivares <cristobal@cxsoftware.com>
2017-04-12 03:22:06 -0300

    Using new class EratosthenesPrimes for proble 10 (not slow anymore!)

--- eulerproject/Problems/Problem10.cs
+++ eulerproject/Problems/Problem10.cs
@@ -2,14 +2,12 @@
 
 namespace eulerproject
 {
-	[Slow]
 	[Problem (10, "Summation of primes", 142913828922)]
 	public class Problem10 : IProblem
 	{
 		public long Run ()
 		{
-			return Primes.Get ()
-				.TakeWhile (x => x < 2000000)
+			return EratosthenesPrimes.Get (2000000 - 1)
 				.Sum ();
 		}
 	}
